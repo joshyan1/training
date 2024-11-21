d2173cec8186580306b9b422c0a052256ccfdc2f
joshyan1 <jyan00017@gmail.com>
2024-11-21 00:07:08 -0500

    initial leader

---
+++ distributed/leader.py
@@ -0,0 +1,51 @@
+import threading
+import torch
+import logging
+from ../models/tut_model import Net
+
+device = torch.device("mps")
+
+class Leader():
+    def __init__(self, learners, model, epochs, batch_size):
+        self.lock = threading.Lock()
+        self.max_learners = learners
+        self.learners = []
+        self.gradients = []
+        self.model = model
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.optimizer = optimizer_function(self.model)
+        logging.info("Leader service initialized")
+        initialize_leader()
+
+    def initialize_leader():
+        #set up GRPC
+
+        #start server
+
+    def AddLearner(self, )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Distributed Training Leader')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    parser.add_argument('--learners', type=int, default=0, metavar='L',
+                        help=`Learner Count`)
+    args = parser.parse_args()
+
+    model = Net()
+    leader = Leader(args.learners, model, args.epochs, args.batch_size)
+
+
--- models/tut_model.py
+++ models/tut_model.py
@@ -5,26 +5,7 @@ import functools
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
 
-
-from torch.optim.lr_scheduler import StepLR
-
-import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data.distributed import DistributedSampler
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp.fully_sharded_data_parallel import (
-    CPUOffload,
-    BackwardPrefetch,
-)
-from torch.distributed.fsdp.wrap import (
-    size_based_auto_wrap_policy,
-    enable_wrap,
-    wrap,
-)
 
 class Net(Model):
     def __init__(self):
@@ -73,3 +54,6 @@ class Net(Model):
             print(
                 "Train Epoch: {} \tLoss: {:.6f}".format(epoch, ddp_loss[0] / ddp_loss[1])
             )
+
+model = Net()
+criterion = torch.nn.CrossEntropyLoss()
